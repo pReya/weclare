6278e46da99e65d7faf15da9d3b6a2e7cc7c96ac
Moritz Stueckler <moritz.stueckler@gmail.com>
2019-04-04 16:08:40 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6894fa000409aac5be01b9b2da8f812.js"
+  "/precache-manifest.0470cd45ea7d5bd45e641cbd71133271.js"
 );
 
 workbox.clientsClaim();
